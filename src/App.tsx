f179b4c47727e702546a13b7d94fe75315992ddd
Bashir <b_egeh@hotmail.com>
2020-12-31 16:49:01 -0500

    create NewToDo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ToDoList from './components/ToDoList';
+import NewToDo from './components/NewToDo';
 
 const App: React.FC = () => {
   const todos = [{ id: "t1", text: "Finish the course" }];
   return <div className="App">
+    <NewToDo />
     <ToDoList items={todos} />
   </div>;
 };
--- src/components/NewToDo.tsx
+++ src/components/NewToDo.tsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-const NewToDo: React.FC = () => {};
+const NewToDo: React.FC = () => {
+  const textInputRef = useRef<HTMLInputElement>(null);
+
+  const todoSubmitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    const enteredText = textInputRef.current!.value;
+    console.log(enteredText);
+  }
+
+  return <form onSubmit={todoSubmitHandler}>
+    <div>
+      <label htmlFor="todo-text">Todo Text</label>
+      <input type='text' id='todo-text' ref={textInputRef}/>
+      <button type="submit">ADD TODO</button>
+    </div>
+  </form>
+};
 
 export default NewToDo;
